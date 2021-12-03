effa0af80e254e501067088d98f6f2b0810146a5
queochexe <38107389+queochexe@users.noreply.github.com>
2021-12-03 21:05:16 +0100

    Initial commit

---
+++ challenge_optilyz.jsx
@@ -0,0 +1,163 @@
+const Component1: React.VFC = () => {
+    return (
+        <Component1>
+            <background />
+            <form>
+                <Text>Full Name*</Text>
+                <input1 />
+                <Text>Email*</Text>
+                <input2 />
+                <Text>Additional reference or portfolio</Text>
+                <btn>
+                    <Rectangle4 />
+                    <Text>Save</Text>
+                </btn>
+                <input3 />
+                <plus>
+                    <Rectangle4 />
+                    <Text>+</Text>
+                </plus>
+                <Text>https://www.your-link.com</Text>
+                <Text>jane.doe@yourdomain.com</Text>
+                <Text>Jane Doe</Text>
+            </form>
+            <img src="" />
+            <Text>UX Designer@optilyz</Text>
+        </Component1>
+    )
+}
+
+const Component1 = styled.div`
+    height: 906px;
+    width: 1004px;
+    background-color: #ffffff;
+  `
+const background = styled.div`
+    border-radius: 0.5625rem;
+    height: 906px;
+    width: 1004px;
+    background-color: #ffffff;
+  `
+const form = styled.div`
+    height: 372px;
+    width: 581px;
+  `
+const text1 = styled.div`
+    text-align: left;
+    vertical-align: top;
+    font-size: 14px;
+    font-family: Nunito;
+    line-height: auto;
+    color: #333333;
+  `
+const input1 = styled.div`
+    border-radius: 0.1875rem;
+    height: 39px;
+    width: 579px;
+    background-color: #f1f1f1;
+    border: 0.0625rem solid #e6e6e6;
+  `
+const text2 = styled.div`
+    text-align: left;
+    vertical-align: top;
+    font-size: 14px;
+    font-family: Nunito;
+    line-height: auto;
+    color: #333333;
+  `
+const input2 = styled.div`
+    border-radius: 0.1875rem;
+    height: 39px;
+    width: 579px;
+    background-color: #f1f1f1;
+    border: 0.0625rem solid #e6e6e6;
+  `
+const text3 = styled.div`
+    text-align: left;
+    vertical-align: top;
+    font-size: 14px;
+    font-family: Nunito;
+    line-height: auto;
+    color: #333333;
+  `
+const btn = styled.div`
+    height: 46px;
+    width: 581px;
+  `
+const Rectangle4 = styled.div`
+    border-radius: 0.1875rem;
+    height: 46px;
+    width: 581px;
+    background-color: #93c4ff;
+  `
+const text4 = styled.div`
+    text-align: left;
+    vertical-align: top;
+    font-size: 18px;
+    font-family: Nunito;
+    line-height: auto;
+    color: #ffffff;
+  `
+const input3 = styled.div`
+    border-radius: 0.1875rem;
+    height: 39px;
+    width: 516px;
+    background-color: #f1f1f1;
+    border: 0.0625rem solid #e6e6e6;
+  `
+const plus = styled.div`
+    height: 39px;
+    width: 44px;
+  `
+const Rectangle4 = styled.div`
+    border-radius: 0.1875rem;
+    height: 39px;
+    width: 44px;
+    background-color: #93c4ff;
+  `
+const text5 = styled.div`
+    text-align: left;
+    vertical-align: top;
+    font-size: 22px;
+    font-family: Nunito;
+    line-height: auto;
+    color: #ffffff;
+  `
+const text6 = styled.div`
+    text-align: left;
+    vertical-align: top;
+    font-size: 14px;
+    font-family: Nunito;
+    line-height: auto;
+    text-decoration: underline;
+    color: #999999;
+  `
+const text7 = styled.div`
+    text-align: left;
+    vertical-align: top;
+    font-size: 14px;
+    font-family: Nunito;
+    line-height: auto;
+    text-decoration: underline;
+    color: #999999;
+  `
+const text8 = styled.div`
+    text-align: left;
+    vertical-align: top;
+    font-size: 14px;
+    font-family: Nunito;
+    line-height: auto;
+    color: #999999;
+  `
+const img9 = styled.div`
+    height: 75px;
+    width: 229px;
+  `
+const text10 = styled.div`
+    text-align: left;
+    vertical-align: top;
+    font-size: 34px;
+    font-family: Nunito;
+    line-height: auto;
+    color: #333333;
+  `
